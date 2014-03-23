1f80c46ed32c2ffb99e1d1d6c6a017035ca94f39
Daniel Mackay <crankage@gmail.com>
2014-03-23 12:13:26 +1100

    Initial implementation.

---
+++ FreshCss/05_Spin-Logon/script.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+  var formContainer = $('#formContainer');
+
+  // Listening for clicks on the ribbon links
+  $('.flipLink').click(function (e) {
+    // Flipping the forms
+    formContainer.toggleClass('flipped');
+
+    // If there is no CSS3 #D support simply hide the
+    // login form (exposing the recover one)
+    if (Modernizr.csstransforms3d) {
+      $('#login').toggle();
+      //alert('feature detected!');
+    }
+
+    e.preventDefault();
+
+  });
+
+  formContainer.find('form').submit(function (e) {
+    // Preventing form submissions.  If you implement a backend,
+    // you will want to remove this code.
+  });
+});
+
---
+++ FreshCss/06_ProductShowcase/js/script.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+  var imp = impress();
+
+  $('#arrowLeft').click(function() {
+    imp.prev();
+    E.preventDefault();
+  });
+  
+  $('#arrowRight').click(function () {
+    imp.next();
+    E.preventDefault();
+  });
+
+});
